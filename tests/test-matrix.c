25e1a18db412f68de8e1e681521e4c2e6cb9de97
chenweiii <c.weiii94@gmail.com>
2017-04-11 13:20:05 +0800

    Add function producing matrix randomly
    Use the result from naive impl. to veify the correction of other impl.

--- tests/test-matrix.c
+++ tests/test-matrix.c
@@ -1,5 +1,11 @@
 #include "matrix.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define TEST_ROW 24
+#define TEST_COL 24
+#define SAMPLES_NUM 100
 
 MatrixAlgo *matrix_providers[] = {
     &NaiveMatrixProvider,
@@ -9,36 +15,31 @@ MatrixAlgo *matrix_providers[] = {
     &prefetchAVXMatrixProvider,
 };
 
+int *random_matrix(int row, int col)
+{
+    int *src = (int *) malloc(row * col * sizeof(int));
+    srand(time(NULL));
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < col; j++)
+            *(src + i * col + j) = rand();
+    return src;
+}
+
 int main()
 {
     MatrixAlgo *algo = matrix_providers[4];
+    MatrixAlgo *verify = matrix_providers[0];
 
     Matrix *dst, *m, *n, *fixed;
 
-    m = algo->create(4, 3);
-    algo->assign(m, (int []) {
-        1, 2, 3,
-        5, 6, 7,
-        1, 2, 3,
-        5, 6, 7,
-    });
+    m = algo->create(TEST_ROW, TEST_COL);
+    algo->assign(m, random_matrix(TEST_ROW, TEST_COL));
 
-    n = algo->create(3, 4);
-    algo->assign(n, (int []) {
-        1, 2, 3, 4,
-        5, 6, 7, 8,
-        1, 2, 3, 4
-    });
+    n = algo->create(TEST_ROW, TEST_COL);
+    algo->assign(n, random_matrix(TEST_ROW, TEST_COL));
 
     algo->mul(&dst, m, n);
-
-    fixed = algo->create(4, 4);
-    algo->assign(fixed, (int []) {
-        14, 20, 26, 32,
-        42, 60, 78, 96,
-        14, 20, 26, 32,
-        42, 60, 78, 96
-    });
+    verify->mul(&fixed, m, n);
 
     if (algo->equal(dst, fixed))
         return 0;
